2912a31435900759c14802fe068f7b583442f070
Nguyen Nhat <nldnhat182006@gmail.com>
2026-04-03 13:50:21 +0700

    feat: add getAllReaders for ReaderService

--- src/service/ReaderService.java
+++ src/service/ReaderService.java
@@ -1,4 +1,24 @@
 package service;
 
+import model.Reader;
+import util.FileManager;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ReaderService {
+
+    private final FileManager  fileManager;
+    private List<Reader>       readers;
+
+    public ReaderService(FileManager fileManager) {
+        this.fileManager = fileManager;
+        this.readers     = fileManager.readReaders();
+    }
+
+    public List<Reader> getAllReaders() {
+        return new ArrayList<>(readers);
+    }
+
+
 }
